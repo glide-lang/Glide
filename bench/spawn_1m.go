2027829bb0106672968af9f2bc12a8d3587c8bc1
Murilinho145SG <bye456oi@gmail.com>
2026-05-05 16:19:09 -0300

    bench: spawn_pure, spawn_1m, chan_i64 vs Go

---
+++ bench/spawn_1m.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+var pending int32
+
+func worker() {
+	atomic.AddInt32(&pending, -1)
+}
+
+func main() {
+	const n = 1000000
+	atomic.StoreInt32(&pending, int32(n))
+	t0 := time.Now()
+	for i := 0; i < n; i++ {
+		go worker()
+	}
+	for atomic.LoadInt32(&pending) > 0 {
+	}
+	d := time.Since(t0)
+	fmt.Printf("spawn+drain %d: %d ms\n", n, d.Milliseconds())
+}
